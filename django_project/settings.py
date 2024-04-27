2b6aee7e705c0a1d71734f9d5f456d5168983354
myasoedas <myasoedas@yandex.ru>
2024-04-27 22:27:08 +0300

    fetch: Добавить стили в файл myfirst.css в папке static в корне приложения members

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,4 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
